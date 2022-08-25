29ebae61b8bd3a7d654df3858c0f0d05ee2ceb2c
Attar Rangga Hardiansyah <attarranggaaa@gmail.com>
2022-08-26 01:40:55 +0700

    small changes

--- components/Links.jsx
+++ components/Links.jsx
@@ -9,8 +9,8 @@ const Links = () => {
       <div className="space-y-5">
         <div className="grid grid-cols-4 text-sm gap-5 text-zinc-400">
           <div className="space-y-2">
-            {links.map((link) => (
-              <h1>{link.title}</h1>
+            {links.map((link,i) => (
+              <h1 key={i}>{link.title}</h1>
             ))}
           </div>
           <div className="col-span-3 space-y-2 text-white">
